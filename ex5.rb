1e6b083a0e6c4b84d343cff38a4c19abad69bca4
David Kong <xlimitlessskiesx@gmail.com>
2017-03-21 21:20:52 -0400

    Finished up to ex5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'David Kong'
+my_age = 23
+my_height = 5 * 12 + 11
+my_weight = 178 
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} poundes heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add my #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
